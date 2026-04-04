39a81f7d5566c4126caf568ba4fe8efd9f34ea23
z3r0x3ch0 <z3r0x3ch0@gmail.com>
2026-04-03 19:57:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 <script type='text/javascript' src='config.js'></script>
 <script type='text/javascript' src='script.js'></script>   
+fetch('http://localhost:3001/api/data')
+  .then(response => response.json())
+  .then(data => console.log(data));   
